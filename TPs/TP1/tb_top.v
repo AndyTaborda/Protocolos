4dcb5cfe452eaf2a635a4ee97fadffa63374db26
AndyTaborda <andrea.taborda@mi.unc.edu.ar>
2025-06-10 02:29:26 -0300

    Cambia a lógica no bloqueante
    Ahora anda todo

--- TPs/TP1/tb_top.v
+++ TPs/TP1/tb_top.v
@@ -27,78 +27,110 @@ module tb_top();    //No tienen puertos
     // Prueba funcionamiento reset
     // ==============================
     `ifdef TEST_1
+        integer reset_delay;
+
         initial begin
             //Arrancan desde t=0
-                   i_reset    = 1'b0  ;  //Reseteado
-                   clock      = 1'b0  ;  //Clock en 0
-                   i_sw[0]    = 1'b0  ;  //Contador deshabilitado
-                   i_sw[2:1]  = 2'h0  ;  //Velocidad R0
-                   i_sw[3]    = 1'b0  ;  //Color azul
+                   i_reset    <= 1'b0  ;  //Reseteado
+                   clock      <= 1'b0  ;  //Clock en 0
+                   i_sw[0]    <= 1'b0  ;  //Contador deshabilitado
+                   i_sw[2:1]  <= 2'h0  ;  //Velocidad R0
+                   i_sw[3]    <= 1'b0  ;  //Color azul
+                   
+            #100   i_reset    <= 1'b1  ;  //Sale de reset
+                   i_sw[0]    <= 1'b1  ;  //Contador habilitado
 
-            #100   i_reset    = 1'b1  ;  //Sale de reset
-                   i_sw[0]    = 1'b1  ;  //Contador habilitado
-
-            //Espera algunos ciclos para que los LEDs cambien
-            #2000 i_reset = 1'b0;       //Se resetea
-            #10                         //Se a que se aplique el reset
+            // Se repite el test 5 veces con tiempos aleatorios
+            repeat (5)
+                begin
+                    //Se espera algunos ciclos para que los LEDs cambien
+                    reset_delay = $urandom_range(1, 2000);
+                    #reset_delay    i_reset = 1'b0;   //Se resetea
+                    #10                           ;   //Se espera a que se aplique el reset
     
-            //Se verificamos que los LEDs azules vuelvan a 4'b0001
-            if (o_led !== {{NB_LEDS-1{1'b0}},1'b1})
-            begin
-                $display("=================================================");                
-                $display("TEST 1 FAILED: o_led no se reinició correctamente. Valor: %b", o_led);
-                $display("=================================================");
-                $finish;
-            end
-            $display("=================================================");
-            $display("TEST 1 PASSED: Reset correcto, o_led = %b", o_led);
-            $display("=================================================");
+                    //Se verifica que los LEDs azules vuelvan a 4'b0001
+                    if (o_led !== {{NB_LEDS-1{1'b0}},1'b1})
+                    begin
+                        $display("=================================================");                
+                        $display("TEST 1 FAILED: o_led no se reinició correctamente. Valor: %b", o_led);
+                        $display("=================================================");
+                        $finish;
+                    end
+                    else
+                    begin
+                        $display("Corriendo test...");
+                    end
+                    #100   i_reset    <= 1'b1  ;  //Sale de reset
 
+                end
+            $display("=================================================");
+            $display("TEST 1 PASSED: Reset correcto, o_led = %b", o_led );
+            $display("=================================================");
             #100 $finish;
+            
         end
     `endif
-
 
 
     // =========== TEST 2 ===========
     // Prueba funcionamiento de leds
     // ==============================
-   `ifdef TEST_2        
+   `ifdef TEST_2       
+        integer delay_led;
+  
         initial
         begin
             //Arrancan desde t=0
-                   i_reset       = 1'b0   ;  //Reseteado
-                   clock         = 1'b0   ;  //Clock en 0
-                   i_sw[0]       = 1'b0   ;  //Contador deshabilitado
-                   i_sw[2:1]     = 2'h0   ;  //Velocidad R0
-                   i_sw[3]       = 1'b0   ;  //Color azul
-
-            #100   i_reset       = 1'b1   ;  //Sale de reset
-            #100   i_sw[0]       = 1'b1   ;  //Habilita el contador
+                   i_reset      <= 1'b0   ;  //Reseteado
+                   clock        <= 1'b0   ;  //Clock en 0
+                   i_sw[0]      <= 1'b0   ;  //Contador deshabilitado
+                   i_sw[2:1]    <= 2'h0   ;  //Velocidad R0
+                   i_sw[3]      <= 1'b0   ;  //Color azul
+            
+            #100   i_reset      <= 1'b1   ;  //Sale de reset
+            #100   i_sw[0]      <= 1'b1   ;  //Habilita el contador
                    
-            //Prueba en color azul (i_sw[3] = 0)
-            #1000;
-            if (o_led_b !== o_led || o_led_g !== {NB_LEDS{1'b0}})
+            repeat(5)
             begin
-                $display("=================================================");
-                $display("ERROR: LEDS no muestran azul correctamente");
-                $display("=================================================");
-                $finish;
-            end
-        
-            //Prueba en color verde (i_sw[3] = 1)
-            #100   i_sw[3] = 1'b1;
-            #1000;
-            if (o_led_g !== o_led || o_led_b !== {NB_LEDS{1'b0}})
-            begin
-                $display("=================================================");
-                $display("ERROR: LEDS no muestran verde correctamente");
-                $display("=================================================");
-                $finish;
+                //Prueba en color azul (i_sw[3] = 0)
+                //Se espera algunos ciclos para que los LEDs cambien
+                delay_led = $urandom_range(1, 2000);
+                #delay_led                         ;
+
+                if (o_led_b !== o_led || o_led_g !== {NB_LEDS{1'b0}})
+                begin
+                    $display("=================================================");
+                    $display("ERROR: LEDS no muestran azul correctamente"       );
+                    $display("=================================================");
+                    $finish;
+                end
+                else
+                begin
+                    $display("Corriendo test...");
+                end
+            
+                //Prueba en color verde (i_sw[3] = 1)
+                #10   i_sw[3] <= 1'b1;
+                #delay_led            ;
+
+                if (o_led_g !== o_led || o_led_b !== {NB_LEDS{1'b0}})
+                begin
+                    $display("=================================================");
+                    $display("ERROR: LEDS no muestran verde correctamente"      );
+                    $display("=================================================");
+                    $finish;
+                end
+                else
+                begin
+                    $display("Corriendo test...");
+                end
+
+                #10    i_sw[3] <= 1'b0;  //Vuelve a cambiar a color azul
+
             end
 
             $display("=================================================");
-            $display("TEST 2 PASSED: Cambio de color correcto");
+            $display("TEST 2 PASSED: Cambio de color correcto"          );
             $display("=================================================");
             #50 $finish;
         end   
@@ -161,9 +193,9 @@ module tb_top();    //No tienen puertos
                 end
                 prev_value = o_led;
             end
-            $display("=================================================");
+            $display("=================================================" );
             $display("TEST 3 PASSED: Corrimiento correcto a velocidad R0");
-            $display("=================================================");
+            $display("=================================================" );
             $finish;
         end
     `endif
@@ -174,44 +206,54 @@ module tb_top();    //No tienen puertos
     // ==============================
     `ifdef TEST_4
         //Crea variable auxiliar
-        reg [NB_LEDS-1:0] val_led     ;  
+        reg     [NB_LEDS-1:0] val_led ;  
+        integer delay_sw              ;
 
         initial
         begin
             //Arrancan desde t=0
-                   i_reset    = 1'b0  ;  //Reseteado
-                   clock      = 1'b0  ;  //Clock en 0
-                   i_sw[0]    = 1'b0  ;  //Contador deshabilitado
-                   i_sw[2:1]  = 2'h0  ;  //Velocidad R0
-                   i_sw[3]    = 1'b0  ;  //Color azul
+                   i_reset    <= 1'b0  ;  //Reseteado
+                   clock      <= 1'b0  ;  //Clock en 0
+                   i_sw[0]    <= 1'b0  ;  //Contador deshabilitado
+                   i_sw[2:1]  <= 2'h0  ;  //Velocidad R0
+                   i_sw[3]    <= 1'b0  ;  //Color azul
 
-            #100   i_reset    = 1'b1  ;  //Sale de reset
-            #100   i_sw[0]    = 1'b1  ;  //Contador habilitado
+            #100   i_reset    <= 1'b1  ;  //Sale de reset
+            #100   i_sw[0]    <= 1'b1  ;  //Contador habilitado
 
-            //Espera algunos ciclos
-            repeat (2 * (2**(NB_COUNT-10))) @(posedge clock);
-            #20
-                   val_led    = o_led ;  //Guarda el estado de los leds
-                   i_sw[0]    = 1'b0  ;  //Contador deshabilitado
-            
-            //Espera algunos ciclos
-            repeat (2 * (2**(NB_COUNT-10))) @(posedge clock);
-            #20
-
-            if (o_led == val_led)
+            repeat(5)
             begin
-                $display("=================================================");
-                $display("TEST 4 PASSED: i_sw[0] controla el contador");
-                $display("=================================================");
+                //Espera algunos ciclos
+                 delay_sw = $urandom_range(100, 2000);
+                #delay_sw                            ;
+
+                    val_led    <= o_led ;  //Guarda el estado de los leds
+                    i_sw[0]    <= 1'b0  ;  //Contador deshabilitado
+                
+                //Espera algunos ciclos
+                 delay_sw = $urandom_range(100, 2000);
+                #delay_sw 
+
+                if (o_led !== val_led)
+                begin
+                    $display("=================================================");
+                    $display("TEST 4 FAILED: o_led cambió sin enable"           );
+                    $display("=================================================");
+                    $finish;
+                end
+                else
+                begin
+                    $display("Corriendo test..."                                );
+                end
+
+                i_sw[0]    <= 1'b1  ;  //Contador habilitado
+
             end
-
-            else
-            begin
                 $display("=================================================");
-                $display("TEST 4 FAILED: o_led cambió sin enable");
+                $display("TEST 4 PASSED: i_sw[0] controla el contador"      );
                 $display("=================================================");
                 $finish;
-            end
+            
         
             #50 $finish;
         end
@@ -228,24 +270,31 @@ module tb_top();    //No tienen puertos
         localparam R3 = 2**(NB_COUNT-13)-1; 
 
         //Crea variable auxiliar
-        reg [NB_LEDS-1:0] val_led         ; 
+        reg [NB_LEDS   - 1 : 0] val_led   ; 
         reg [NB_COUNT  - 1 : 0] count     ;
         reg [NB_COUNT  - 1 : 0] exp_count ;
 
         initial
         begin
             //Arrancan desde t=0
-                   i_reset    = 1'b0  ;  //Reseteado
-                   clock      = 1'b0  ;  //Clock en 0
-                   i_sw[0]    = 1'b0  ;  //Contador deshabilitado
-                   i_sw[2:1]  = 2'b11 ;  //Velocidad R0
-                   i_sw[3]    = 1'b0  ;  //Color azul
-                   count      = 1'b0  ;  //Inicializa contador en 0
-
+                   i_reset    <= 1'b0  ;  //Reseteado
+                   clock      <= 1'b0  ;  //Clock en 0
+                   i_sw[0]    <= 1'b0  ;  //Contador deshabilitado
+                   i_sw[2:1]   = 2'b11 ;  //Velocidad R0
+                   i_sw[3]    <= 1'b0  ;  //Color azul
+                   count       = 1'b0  ;  //Inicializa contador en 0
+                   
+                   //Setea valor esperado según velocidad seleccionada
+                   case (i_sw[2:1])
+                        2'b00: exp_count = R0 + 1'b1;
+                        2'b01: exp_count = R1 + 1'b1;
+                        2'b10: exp_count = R2 + 1'b1;
+                        2'b11: exp_count = R3 + 1'b1;
+                   endcase
 
             #100
                    @(posedge clock)   ;  //Espera al flanco del clock
-                   i_reset    = 1'b1  ;  //Sale de reset
+                   i_reset   = 1'b1   ;  //Sale de reset
 
                    @(posedge clock)   ;  //Espera al flanco del clock
                    i_sw[0]    = 1'b1  ;  //Contador habilitado
@@ -253,27 +302,24 @@ module tb_top();    //No tienen puertos
 
             
             //Espera a que haya corrimiento
-            //while (o_led != {val_led[NB_LEDS-2:0], val_led[NB_LEDS-1]})
-            while (o_led == val_led)
+            @(posedge clock)      ;
+            while (o_led != {val_led[NB_LEDS-2:0], val_led[NB_LEDS-1]})
+            
+            //while (o_led == val_led)
             begin
-                @(posedge clock)      ;
                 count = count + 1'b1  ;
+                $display("o_led = %h - val_led = %h", o_led, val_led);
+                @(posedge clock)      ;
             end
 
-            //Setea valor esperado según velocidad seleccionada
-            case (i_sw[2:1])
-                2'b00: exp_count = R0;
-                2'b01: exp_count = R1;
-                2'b10: exp_count = R2;
-                2'b11: exp_count = R3;
-            endcase
-
+            #1000                     ;
             //Verifica resultado 
             if (count == exp_count)
             begin
                 $display("=================================================");
                 $display("TEST 5 PASSED: Corrimiento tras %0d ciclos", count);
                 $display("=================================================");
+                $finish;
 
             end
             else
@@ -281,11 +327,10 @@ module tb_top();    //No tienen puertos
                 $display("=================================================");
                 $display("TEST 5 FAILED: Esperado %0d ciclos, pero fue %0d", exp_count, count);
                 $display("=================================================");
+                $finish;
             end
-            #50 $finish;
         end
     `endif
-
 
 
   //Swiching: periodo de 10 ns ___---___---___
